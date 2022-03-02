349221bed4676664cd25fe0d87e165bc7d7224b2
hongquan1811 <daohongquan1811@gmail.com>
2022-03-02 21:22:47 +0700

    update

--- ecomerce/stores/admin.py
+++ ecomerce/stores/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 from django.utils.html import format_html
 from .models import Category, Publisher, Author, Book, BookItem
 
+from django.contrib import admin
+from django.utils.html import format_html
+from .models import Category, Publisher, Author, Book, BookItem , Mobilephone, Mobilephone_item, Mobilephone_manufacture
+
+class MobilephoneAdmin(admin.ModelAdmin):
+    list_display = ['name', 'screen', 'model', 'ram']
+
+class Mobilephone_manufactureAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address', 'email', 'phone']
+class Mobilephone_itemAdmin(admin.ModelAdmin):
+
+    def image_view(self, obj):
+        return format_html('<img src="{}" width="200" height="200" />'.format(obj.image.url))
+    
+    def mobilephone_name(self, obj):
+        return obj.mobilephone.name
+    
+    image_view.short_description = 'IMAGE'
+    mobilephone_name.short_description = 'NAME'
+    list_display = ['image_view', 'mobilephone_name', 'price', 'discount']
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ['name', 'address', 'biography']
@@ -33,3 +53,6 @@ admin.site.register(Publisher, PublisherAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Book, BookAdmin)
 admin.site.register(BookItem, BookItemAdmin)
+admin.site.register(Mobilephone, MobilephoneAdmin)
+admin.site.register(Mobilephone_item, Mobilephone_itemAdmin)
+admin.site.register(Mobilephone_manufacture, Mobilephone_manufactureAdmin)
--- ecomerce/stores/models/__init__.py
+++ ecomerce/stores/models/__init__.py
@@ -3,3 +3,6 @@ from .book.category import Category
 from .book.publisher import Publisher
 from .book.book import Book
 from .book.book_item import BookItem
+from .mobilephone.mobilephone import Mobilephone
+from .mobilephone.mobilephone_item import Mobilephone_item
+from .mobilephone.mobilephone_manufacture import Mobilephone_manufacture
